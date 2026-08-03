9c86eea7173ae85a513fefdeea09d5348e0a2b1f
Haley <wolfhaleydewd@gmail.com>
2026-08-03 19:54:35 -0400

    Updated the ship speed and allowed bullets

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
 
         # Alien settings
         self.fleet_drop_speed = 10
@@ -38,7 +38,7 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
         self.bullet_speed = 2.5
         self.alien_speed = 1.0
 
